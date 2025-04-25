4fe425c5cd5fd38ef9d32a66b5bc72fd135a4cb3
phs <liaparkel@gmail.com>
2025-04-25 14:44:46 +0900

    feat: SprtService 생성

---
+++ src/com/bok/service/SprtService.java
@@ -0,0 +1,82 @@
+package com.bok.service;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import com.bok.model.DBCP;
+import com.bok.model.SprtDAO;
+import com.bok.model.SprtInfoVO;
+import com.bok.model.SprtPersonVO;
+import com.bok.model.SprtContentVO;
+import com.bok.model.PagingVO;
+
+public class SprtService {
+
+	private SprtDAO dao;
+
+	public SprtService() {
+		dao = new SprtDAO();
+	}
+
+	// 카테고리 가져오기
+	public List<SprtInfoVO> getSprtInfo() {
+		try (SqlSession session = DBCP.getSqlSessionFactory().openSession()) {
+			return dao.getSprtInfo(session);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// 사용자에게 오늘 기준으로 유효한 지원금 정보 제공
+	public List<SprtPersonVO> getSprtPerson(int fkSprtNum) {
+		try (SqlSession session = DBCP.getSqlSessionFactory().openSession()) {
+			return dao.getSprtPerson(session, fkSprtNum);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// 제목 아코디언 버튼 클릭 시 개별 내용 제공
+	public List<SprtContentVO> getSprtContent(int fkSprtpNum) {
+		try (SqlSession session = DBCP.getSqlSessionFactory().openSession()) {
+			return dao.getSprtContent(session, fkSprtpNum);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// 사용자에게 이전 지원금 정보 제공(종료일 내림차순)
+	public List<SprtPersonVO> getBfSprtPerson(int fkSprtNum) {
+		try (SqlSession session = DBCP.getSqlSessionFactory().openSession()) {
+			return dao.getBfSprtPerson(session, fkSprtNum);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// 사용자에게 카테고리에 해당하는 이전 지원금 정보 제공(종료일 내림차순, Pagination)
+	public List<SprtPersonVO> getPagedBfSprtPerson(int fkSprtNum, PagingVO paging) {
+		try (SqlSession session = DBCP.getSqlSessionFactory().openSession()) {
+			return dao.getPagedBfSprtPerson(session, fkSprtNum, paging);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	// 카테고리에 해당하는 전체 게시물 수 조회
+	public int getBfSprtPersonCount(int fkSprtNum) {
+		try (SqlSession session = DBCP.getSqlSessionFactory().openSession()) {
+			return dao.getBfSprtPersonCount(session, fkSprtNum);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0;
+		}
+	}
+}
