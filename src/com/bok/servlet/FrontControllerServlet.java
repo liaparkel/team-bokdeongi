0ca00f7535d8fbda4a8dac16acd3882a28675127
hyeonaa02 <jeonglucy00@gmail.com>
2025-04-29 10:09:11 +0900

    feat : servlet 한글 변환

--- src/com/bok/servlet/FrontControllerServlet.java
+++ src/com/bok/servlet/FrontControllerServlet.java
@@ -12,6 +12,8 @@ public class FrontControllerServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		
 		String cmd = request.getParameter("cmd");
 		if (cmd == null) cmd = "mainUI";
 
